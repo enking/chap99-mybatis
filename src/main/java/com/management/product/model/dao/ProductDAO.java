9971368eee1dca20eb2a844e17238667922af011
enking <engur9902@gmail.com>
2024-09-08 00:12:20 +0900

    First commit

---
+++ src/main/java/com/management/product/model/dao/ProductDAO.java
@@ -0,0 +1,10 @@
+package com.management.product.model.dao;
+
+import com.management.product.model.dto.ProductDTO;
+
+import java.util.List;
+
+public interface ProductDAO {
+    List<ProductDTO> selectAllProductList();
+    // * 주석을 지우고 Mapper interface(= DAO class) 역할을 하도록 내용을 작성하세요.
+}
